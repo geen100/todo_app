592a42b8b3457e859dbaab656c72b9e914d896fb
geen100 <20122055@kaishi-pu.ac.jp>
2024-08-29 11:39:46 +0900

    make users table

--- main.go
+++ main.go
@@ -8,11 +8,25 @@ import (
 func main() {
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "example@a.com"
-	u.PassWord = "test"
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "example@a.com"
+	// u.PassWord = "test"
+	// fmt.Println(u)
+
+	// u.CreateUser()
+
+	u, _ := models.GetUser(1)
+
 	fmt.Println(u)
 
-	u.CreateUser()
+	u.Name = "yugen"
+	u.Email = "yugen@test"
+	u.UpdateUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+
+	u.DeleteUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
 }
