a4f24bc930a0f62805b327d066026d07c3cc45ad
geen100 <gen.10.07.yu@icloud.com>
2024-09-04 18:45:33 +0900

    createn user session

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,15 +1,25 @@
 package controllers
 
 import (
-	"log"
 	"net/http"
-	"text/template"
 )
 
-func Top(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("app/views/templets/top.html")
-	if err != nil{
-		log.Fatalln(err)
+func top(w http.ResponseWriter, r *http.Request) {
+	_, err := session(w, r)
+	if err != nil {
+		generateHTML(w, nil, "layout", "public_navbar", "top")
+	} else {
+		http.Redirect(w, r, "/todos", 302)
 	}
-	t.Execute(w, "Hello")
+}
+
+
+
+func index(w http.ResponseWriter, r * http.Request){
+	_,err := session(w,r)
+	if err != nil{
+		http.Redirect(w,r,"/",302)
+	} else{
+		generateHTML(w,nil,"layout","private_navbar","index")
+	}
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,13 +1,43 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"text/template"
+	"todo_app/app/models"
 	"todo_app/config"
 )
 
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
+
+
+func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err error) {
+	cookie, err := r.Cookie("_cookie")
+	if err == nil {
+		sess = models.Session{UUID: cookie.Value}
+		if ok, _ := sess.CheckSession(); !ok {
+			err = fmt.Errorf("Invalid session")
+		}
+	}
+	return sess, err
+}
+
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
-	http.Handle("/static/", http.StripPrefix("/static/",files))
-	http.HandleFunc("/", Top)
+	http.Handle("/static/", http.StripPrefix("/static/", files))
+	http.HandleFunc("/", top)
+	http.HandleFunc("/signup", Signup)
+	http.HandleFunc("/login", Login)
+	http.HandleFunc("/authenticate", authenticate)
+	http.HandleFunc("/logout",logout)
+	http.HandleFunc("/todos",index)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
